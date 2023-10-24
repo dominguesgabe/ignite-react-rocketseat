75c43328c7eedb0cdcd99e4186da85ca4df83109
dominguesgabe <gabrieldomingues522@gmail.com>
2023-10-24 17:15:13 -0300

    stitches setup

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+})
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
